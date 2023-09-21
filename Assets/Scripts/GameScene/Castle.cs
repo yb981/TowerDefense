f7f0657acfd530ab02e9019a2b4491690aedef7c
yb981 <113829864+yb981@users.noreply.github.com>
2023-09-21 11:27:22 +0200

    new castle sprites

--- Assets/Scripts/GameScene/Castle.cs
+++ Assets/Scripts/GameScene/Castle.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Castle : MonoBehaviour
 {
-    [SerializeField] private Transform castlePrefab;
+    [SerializeField] private Transform castleFrontPrefab;
+    [SerializeField] private Transform castleVerticalLeftPrefab;
+    [SerializeField] private Transform castleVerticalRightPrefab;
+    [SerializeField] private Transform castleTopRightPrefab;
+    [SerializeField] private Transform castleBottomRightPrefab;
+    [SerializeField] private Transform castleTopLeftPrefab;
+    [SerializeField] private Transform castleBottomLeftPrefab;
     private TileGrid tileGrid;
     
     private void Awake() 
@@ -21,16 +27,35 @@ public class Castle : MonoBehaviour
         {
             for (int j = 0; j < height; j++)
             {
-                if(i == 0 || j == 0 || i == width-1 || j == width - 1)
+                if(i == width - 1 && j == width-1) 
                 {
-                    BuildCastleTile(i,j);
+                    BuildCastleTile(castleTopRightPrefab,i,j);
+                }else if(i == width -1 && j == 0)
+                {
+                    BuildCastleTile(castleBottomRightPrefab,i,j);
+                } else if(i == 0 && j == 0)
+                {
+                    BuildCastleTile(castleBottomLeftPrefab,i,j);
+                }else if(i == 0 && j == width-1)
+                {
+                    BuildCastleTile(castleTopLeftPrefab,i,j);
+                }
+                else if(j == 0 || j == width-1)
+                {
+                    BuildCastleTile(castleFrontPrefab,i,j);
+                }else if(i == 0)
+                {   
+                    BuildCastleTile(castleVerticalLeftPrefab,i,j);
+                }else if(i == width - 1)
+                {   
+                    BuildCastleTile(castleVerticalRightPrefab,i,j);
                 }
             }
         }
     }
 
-    private void BuildCastleTile(int x, int y)
+    private void BuildCastleTile(Transform prefab,int x, int y)
     {
-        tileGrid.PlaceNewTile(Instantiate(castlePrefab,tileGrid.GetWorldPositionOfCell(x,y),Quaternion.identity,transform),x,y);
+        tileGrid.PlaceNewTile(Instantiate(prefab,tileGrid.GetWorldPositionOfCell(x,y),Quaternion.identity,transform),x,y);
     }
 }
